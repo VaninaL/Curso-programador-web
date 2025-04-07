76f491edfaa2b324e92fed94977cc07b3c2f2c32
santysnk <santysnk@gmail.com>
2025-04-06 21:29:08 -0300

    Cambios en el archivo ejercicio2.ts

--- clase9/ejercicio2.ts
+++ clase9/ejercicio2.ts
@@ -1,20 +1,19 @@
 /* Construya un algoritmo que tenga un arreglo de 
 dimensión 5 y llénelo con los números que el usuario 
 desee.
- • Muestre los números del arreglo al usuario*/
- import* as rls from "readline-sync";
+   • Muestre los números del arreglo al usuario*/
 
- let num1:number[]=new Array(5);  
+import* as rls from "readline-sync";
+
+let num1:number[]=new Array(5);  
 
 
- for (let i:number=0; i<num1.length; i++){
-    num1[i] = rls.questionInt("Ingrese un numero: ");
-    console.log(num1[i]);    
- }
- 
- for (let i:number=0; i<num1.length; i++){
-    console.log(`el numero que ingreso en la posicion ${i} es: ${num1[i]}`);
- }
-    
-    
-    
+for (let i:number=0; i<num1.length; i++){
+   num1[i] = rls.questionInt("Ingrese un numero: ");
+   console.log(num1[i]);    
+}
+
+for (let i:number=0; i<num1.length; i++){
+   console.log(`el numero que ingreso en la posicion ${i} es: ${num1[i]}`);
+}
+
