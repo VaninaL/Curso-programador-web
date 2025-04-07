7cbadddc141cf5ff47d6ba96616307600c593824
vaninaL <vani_labrunee@hotmail.com>
2025-04-07 15:05:30 -0300

    Resolvi el ejercicio 8 del archivo clase 9 realizando cambios al pseudocodigo.

--- clase9/ejercicio8.ts
+++ clase9/ejercicio8.ts
@@ -8,7 +8,7 @@ número es par o impar*/
     1- hacer un arreglo que se va a llamar numeros de tipo number con una longitud 
         de 12 items que van a ser [4,7,9,3,1,45,67,23,29,78,11,16].
 
-    2- hacer uan variable global para almacenar el numero mas grande. 
+    2- hacer una variable global para almacenar el numero mas grande. 
         va a ser de tipo number y se va a llamar numMasGrande.
 
     3- hacer una funcion que se va a llamar parImpar que va a recibir un numero 
@@ -18,3 +18,28 @@ número es par o impar*/
         y lo va a imprimir por consola. ese numero se va a guardar en la variable numMasGrande.
     
 */
+let numeros:number[] = [4,7,9,3,1,45,67,23,29,78,11,16];
+let numMasGrande:number = 1;
+let dimension:number = numeros.length;
+let contador:number = 0;
+
+function parImpar (n:number):string {
+    let respuesta:string;
+    if (n % 2 === 0){
+        respuesta = " El numero ingresado es par "
+    } else {
+        respuesta = " El numero ingresado es impar "
+    }
+    console.log(respuesta);    
+    return respuesta;
+}
+for(let i:number=0; i< dimension; i++){    
+    if (numeros[i]> contador){
+        contador = numeros [i]       
+            
+    }
+}
+console.log ("el numero mas grande es: " + contador);
+numMasGrande = contador;
+parImpar(numMasGrande);
+
