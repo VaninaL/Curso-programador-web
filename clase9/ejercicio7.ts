91742226bbe161c1d1b496a5689159459a9031e8
vaninaL <vani_labrunee@hotmail.com>
2025-04-07 03:06:41 -0300

    Resolvi ejercicio 7 del archivo ejercicio7.ts

--- clase9/ejercicio7.ts
+++ clase9/ejercicio7.ts
@@ -7,14 +7,14 @@ import* as rls from "readline-sync";
 let listaDeLetras:string[] = ["mabel","carla","vanina","melina","lourdes"];
 let nombreBuscado:string = rls.question ("Ingrese el nombre que desea buscar en la lista: ");
 let i:number = 0;
+let verificador:string = "El nombre no se encuentra en el arreglo "
 
-for (i; i<5; i++){
-    
-    if(nombreBuscado == listaDeLetras[i]){
-        console.log("El nombre ingresado se encuentra en el arreglo");        
-    }else {
-        console.log("El nombre ingresado no se encuentra en el arreglo"); 
+for (i; i<5;i++){
+    if (listaDeLetras[i] === nombreBuscado ){
+        verificador = `El nombre coincide con la posicion ${i} del arreglo `;
     }
-
 }
-let vanina:string;
+console.log(verificador);
+console.log(listaDeLetras);
+
+
