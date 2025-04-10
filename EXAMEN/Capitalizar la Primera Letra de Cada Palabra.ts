23163652643d325c6e845edada17716a8d443065
vaninaL <vani_labrunee@hotmail.com>
2025-04-10 14:10:00 -0300

    primer ejercicio del examen listo

---
+++ EXAMEN/Capitalizar la Primera Letra de Cada Palabra.ts
@@ -0,0 +1,17 @@
+// Ejercicio 2: Capitalizar la Primera Letra de Cada Palabra
+// Descripción: Tienes una cadena de texto que contiene varias palabras en minúsculas. 
+// Utiliza split, map, charAt, slice, y join para capitalizar la primera letra de cada palabra.
+
+// Ejemplo: 
+
+let texto = "hola mundo esto es una prueba";
+// Debería convertirse en "Hola Mundo Esto Es Una Prueba"
+
+let arregloTexto:string[] = texto.split(" ");
+
+let primeraLetra:string[] = arregloTexto.map((letra) => letra.charAt(0).toUpperCase() + letra.slice(1));
+
+let textoFinal:string = primeraLetra.join(" ");
+
+console.log(textoFinal);
+
