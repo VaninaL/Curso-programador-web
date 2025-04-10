7911ffcf2b678c731618d19e1e53c0e775738eab
vaninaL <vani_labrunee@hotmail.com>
2025-04-10 14:10:57 -0300

    de vuelta

--- ##EXAMEN##/Capitalizar la Primera Letra de Cada Palabra.ts
+++
@@ -1,9 +0,0 @@
-// Ejercicio 2: Capitalizar la Primera Letra de Cada Palabra
-// Descripción: Tienes una cadena de texto que contiene varias palabras en minúsculas. 
-// Utiliza split, map, charAt, slice, y join para capitalizar la primera letra de cada palabra.
-
-// Ejemplo: 
-
-let texto = "hola mundo esto es una prueba";
-// Debería convertirse en "Hola Mundo Esto Es Una Prueba"
-
